4f4b9a21473e091ec9699cd62c4291b0373ac19d
lizzie <jones@MacBook-Air.local>
2024-09-24 15:19:46 -0400

    fixed warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
